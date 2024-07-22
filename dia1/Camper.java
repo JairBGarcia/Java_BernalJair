6f411fa5a20aa8ab39332de4288e672d9dc78a9b
JairGarcia <156432366+JairBGarcia@users.noreply.github.com>
2024-07-22 08:49:53 -0500

    Feat :rocket: My first day

---
+++ dia1/Camper.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dia1;
+
+/**
+ *
+ * @author camper
+ */
+public class Camper {
+    private int id, telefono;
+    private String nombre, apellido, direccion,acudiente;
+    private boolean riesgo;
+}
+
